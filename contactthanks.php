56d0cc2f9b961ec24f22b86f6ab42d137dbe6a63
Tiffany Mackins <tmackins.1@gmail.com>
2015-04-15 01:23:34 -0400

    4/15

--- contactthanks.php
+++ contactthanks.php
@@ -3,19 +3,13 @@
 <head>
 	<title>A Nice & Simple Contact Form</title>
 	
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="/css/mystyles.css" />
 </head>
 
 <body>
 
 	<div id="page-wrap">
 
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
 			
 		<h1>Your message has been sent!</h1><br />
 		
